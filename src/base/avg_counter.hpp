8e9d9ac41e89e0cd86a47866e284df7f2bed0526
Slava Aseev <sockskilling@gmail.com>
2022-05-10 13:17:54 +0300

    Add some functions to avg_counter

--- src/base/avg_counter.hpp
+++ src/base/avg_counter.hpp
@@ -7,25 +7,44 @@ namespace dfdh {
 template <typename T>
 class avg_counter {
 public:
-    avg_counter(size_t max_count, T pre_calculated_value = T(0), T pre_calculated_count = T(0)):
+    avg_counter(size_t max_count, T pre_calculated_value = T(0), size_t pre_calculated_count = 0):
         _value(pre_calculated_value),
         _count(pre_calculated_count),
-        _max_count(static_cast<T>(max_count)) {}
+        _max_count(max_count) {}
+
+    void reset() {
+        _count = 0;
+    }
 
     void update(T value) {
-        _value = (_value * _count + value) / (_count + 1);
+        auto c = T(_count);
+        _value = (_value * c + value) / (c + 1);
         if (_count + 1 < _max_count)
             _count += 1;
+    }
+
+    void max_count(size_t max_count) {
+        _max_count = max_count;
+        if (_count >= max_count)
+            _count = max_count - 1;
+    }
+
+    [[nodiscard]]
+    size_t max_count() const {
+        return _max_count;
     }
 
     T value() const {
         return _value;
     }
 
-private:
-    T _value;
-    T _count;
-    T _max_count;
-};
+    void value(const T& v) {
+        _value = v;
+    }
 
+private:
+    T      _value;
+    size_t _count;
+    size_t _max_count;
+};
 }
