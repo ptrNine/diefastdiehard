655e13108d12ba49074dcfc2461edb6f2cbae705
Slava Aseev <sockskilling@gmail.com>
2022-05-27 03:22:28 +0300

    Update dfdh.lua script

--- data/scripts/dfdh.lua
+++ data/scripts/dfdh.lua
@@ -2,27 +2,36 @@ local log = require("mod/log").log
 
 cmd [[
     level current lvl_aes
-    sound volume 20
+    sound volume 7
 
     player create 'name=lol group=1'
     player controller0 lol
-    player wpn lol wpn_vss
+    player wpn lol wpn_416
+    ai bind lol
+    ai difficulty lol insane
 
     player create mda
-    player wpn mda wpn_vss
+    player wpn mda wpn_416
     ai bind mda
     ai difficulty mda hard
+]]
+
+--[[
+    ai bind mda
+    ai difficulty mda easy
 
     player create "name=mda1 group=0"
-    player wpn mda1 wpn_vss
+    player wpn mda1 wpn_m40
     ai bind mda1
-    ai difficulty mda1 hard
+    ai difficulty mda1 easy
 
     player create "name=mda2 group=0"
-    player wpn mda2 wpn_vss
+    player wpn mda2 wpn_vssk
     ai bind mda2
-    ai difficulty mda2 hard
+    ai difficulty mda2 easy
+]]
 
+--[[
     player create "name=mda3 group=1"
     player wpn mda3 wpn_vss
     ai bind mda3
