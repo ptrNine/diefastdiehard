5f86b332ccc5a3905c7128336f94ab6b95d84699
Slava Aseev <sockskilling@gmail.com>
2023-01-20 03:11:32 +0300

    base/io: add infd/outfd concepts

--- src/base/io.hpp
+++ src/base/io.hpp
@@ -651,6 +651,37 @@ private:
 DECLARE_OUTFD(outfd, outfd_linux_impl_t);
 DECLARE_OUTFD(outfd_devnull, outfd_devnull_impl);
 
+namespace details {
+    struct autocast {
+        template <typename T>
+        operator T() {
+            return {};
+        }
+    };
+}
+
+template <typename T>
+concept Outfd = requires (T o) {
+    {o.write(details::autocast{}, size_t{})} -> std::same_as<T&>;
+    {o.flatcopy(0)} -> std::same_as<T&>;
+    {o.flush()} -> std::same_as<bool>;
+    {o.seek(size_t{})} -> std::same_as<T&>;
+    {o.put(details::autocast{})} -> std::same_as<T&>;
+    {o.is_fifo()} -> std::same_as<bool>;
+    {o.descriptor()};
+};
+
+template <typename T>
+concept Infd = requires (T i) {
+    {i.read(details::autocast{}, size_t{})} -> std::same_as<io_read_res>;
+    {i.nonblock_read(details::autocast{}, size_t{})} -> std::same_as<io_read_res>;
+    {i.is_blocking()} -> std::same_as<bool>;
+    {i.read_wait_timeout()};
+    {i.read_wait_timeout(details::autocast{})};
+    {i.can_be_blocked()} -> std::same_as<bool>;
+    {i.descriptor()};
+};
+
 template <typename DerivedT, typename Impl, typename T = char, size_t S = 8192, auto... Settings>
 class infd_base_t : public Impl { // NOLINT
 public:
@@ -665,7 +696,7 @@ public:
     constexpr static DerivedT raw_create(fd_type fd) {
         DerivedT infd;
         infd.fd = fd;
-        return std::move(infd);
+        return infd;
     }
 
     infd_base_t() = default;
